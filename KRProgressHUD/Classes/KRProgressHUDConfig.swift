0a70bc470fbdb470ab3332c5de24c69c8c9bbab1
imnotok <bj.y@qq.com>
2025-09-25 10:57:50 +0800

    Create KRProgressHUDConfig.swift

---
+++ KRProgressHUD/Classes/KRProgressHUDConfig.swift
@@ -0,0 +1,16 @@
+//
+//  Untitled.swift
+//  KRProgressHUD
+//
+//  Created by 小毛驴 on 2025/9/25.
+//
+import UIKit
+
+struct KRProgressHUDConfig {
+    static var fadeTime = Double(0.2)
+    static var hudViewMargin = CGFloat(50)
+    static var hudViewPadding = CGFloat(15)
+    static var iconViewSize = CGSize(width: 50, height: 50)
+    static var messageLabelTopMargin = CGFloat(10)
+    static var messageLabelMinWidth = CGFloat(120)
+}
